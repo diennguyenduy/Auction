1ee66fad54152c92fc2f24e51c4e66e6def7ad25
nguyen duy dien <nguyenduydien1311999@gmail.com>
2019-06-24 09:15:56 +0700

    refactor contract code

--- contracts/AuctionBox.sol
+++ contracts/AuctionBox.sol
@@ -1,33 +1,31 @@
-// We will be using Solidity version 0.5.3
 pragma solidity 0.5.3;
 // Importing OpenZeppelin's SafeMath Implementation
 import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract AuctionBox{
-    Auction[] public auctions; 
-   
+contract AuctionBox {
+    Auction[] public auctions;
+
     function createAuction (
         string memory _title,
         uint _startPrice,
         string memory _description
-        ) public{
-        require(_startPrice > 0);
+        ) public {
+        require(startPrice > 0);
         // set the new instanc
         Auction newAuction = new Auction(msg.sender, _title, _startPrice, _description);
         // push the auction address to auctions array
         auctions.push(newAuction);
     }
-    
+
     function returnAllAuctions() public view returns(Auction[] memory){
         return auctions;
     }
 }
 
 contract Auction {
-    
     using SafeMath for uint256;
-    
-    address payable private owner; 
+
+    address payable private owner;
     string title;
     uint startPrice;
     string description;
@@ -38,21 +36,21 @@ contract Auction {
     uint public highestPrice;
     address payable public highestBidder;
     mapping(address => uint) public bids;
-    
+
     /** @dev constructor to creat an auction
       * @param _owner who call createAuction() in AuctionBox contract
       * @param _title the title of the auction
       * @param _startPrice the start price of the auction
       * @param _description the description of the auction
-      */
-      
+    **/
+
     constructor(
         address payable _owner,
         string memory _title,
         uint _startPrice,
         string memory _description
-        
-        ) public {
+        )
+        public {
         // initialize auction
         owner = _owner;
         title = _title;
@@ -60,17 +58,17 @@ contract Auction {
         description = _description;
         auctionState = State.Running;
     }
-    
-    modifier notOwner(){
+
+    modifier notOwner() {
         require(msg.sender != owner);
         _;
     }
-    
+
     /** @dev Function to place a bid
       * @return true
-      */
-    
-    function placeBid() public payable notOwner returns(bool) {
+    **/
+
+    function placeBid() public payable notOwner returns (bool) {
         require(auctionState == State.Running);
         require(msg.value > 0);
         // update the current bid
@@ -82,17 +80,17 @@ contract Auction {
         // update the highest price
         highestPrice = currentBid;
         highestBidder = msg.sender;
-        
+
         return true;
     }
-    
+
     function finalizeAuction() public{
         //the owner and bidders can finalize the auction.
         require(msg.sender == owner || bids[msg.sender] > 0);
-        
+
         address payable recipiant;
         uint value;
-        
+
         // owner can get highestPrice
         if(msg.sender == owner){
             recipiant = owner;
@@ -103,7 +101,7 @@ contract Auction {
             recipiant = highestBidder;
             value = 0;
         }
-        // Other bidders can get back the money 
+        // Other bidders can get back the money
         else {
             recipiant = msg.sender;
             value = bids[msg.sender];
@@ -113,15 +111,18 @@ contract Auction {
         recipiant.transfer(value);
         auctionState = State.Finalized;
     }
-    
+
     /** @dev Function to return the contents od the auction
       * @return the title of the auction
       * @return the start price of the auction
       * @return the description of the auction
-      * @return the state of the auction 
-      */    
-    
-    function returnContents() public view returns(        
+      * @return the state of the auction
+      */
+
+    function returnContents()
+        public
+        view
+        returns(
         string memory,
         uint,
         string memory,
@@ -134,5 +135,4 @@ contract Auction {
             auctionState
         );
     }
-    
 }
